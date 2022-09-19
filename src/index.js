6410c710e6f3ae06a512e8030206ba351be67a08
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-09-19 18:00:48 +0300

    DONE

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+
+function Header(grt) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
